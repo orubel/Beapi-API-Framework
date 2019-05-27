3a5bb97eb5b2bd229bfc1ce30c45de05e330b77f
Owen Rubel <orubel@gmail.com>
2019-05-27 13:04:46 -0700

    test

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -2,23 +2,18 @@
 
 String userHome = System.properties['user.home']
 
-println(System.getenv('BEAPI_BUILD_VERSION'))
-println(System.getenv('BUILD_NUMBER'))
+def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
+def patch = System.getenv('BUILD_NUMBER'))
+def version = "${appVersion}.${patch}"
 
-	def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
-	def patch = (file.text as Integer)-1
-	def version = "${appVersion}.${patch}"
 
-	println(version)
+Properties props = new Properties()
+def propsFile = new File("/home/orubel/.jenkins/workspace/beapi-backend/gradle.properties")
+props.load(propsFile.newDataInputStream())
 
-	/*
-	Properties props = new Properties()
-	def propsFile = new File("/home/orubel/.jenkins/workspace/beapi-backend/gradle.properties")
-	props.load(propsFile.newDataInputStream())
+props.setProperty('apiFrameworkVersion', version)
+props.store(propsFile.newWriter(), null)
 
-	props.setProperty('apiFrameworkVersion', version)
-	props.store(propsFile.newWriter(), null)
-*/
 
 
 
