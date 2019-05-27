c5003891913a09dec9aaf5f66ff02fe0d54fc084
Owen Rubel <orubel@gmail.com>
2019-05-27 12:57:38 -0700

    test

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -1,26 +1,24 @@
 #!/usr/bin/env groovy
 
 String userHome = System.properties['user.home']
-File file = new File("/home/orubel/.jenkins/jobs/api-framework/builds/lastStableBuild/build.xml")
 
-String stringXML = file.text
+println(System.getenv('BEAPI_BUILD_VERSION'))
+println(System.getenv('BUILD_NUMBER'))
 
-def build = new XmlSlurper().parseText(stringXML)
+	def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
+	def patch = (file.text as Integer)-1
+	def version = "${appVersion}.${patch}"
 
+	println(version)
 
+	/*
+	Properties props = new Properties()
+	def propsFile = new File("/home/orubel/.jenkins/workspace/beapi-backend/gradle.properties")
+	props.load(propsFile.newDataInputStream())
 
+	props.setProperty('apiFrameworkVersion', version)
+	props.store(propsFile.newWriter(), null)
+*/
 
-println(build.actions."hudson.plugins.git.util.BuildData".buildsByBranchName.entry."hudson.plugins.git.util.Build".hudsonBuildNumber)
-
-def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
-def patch = build.actions."hudson.plugins.git.util.BuildData".buildsByBranchName.entry."hudson.plugins.git.util.Build".hudsonBuildNumber
-def version = "${appVersion}.${patch}"
-
-Properties props = new Properties()
-def propsFile = new File("/home/orubel/.jenkins/workspace/beapi-backend/gradle.properties")
-props.load(propsFile.newDataInputStream())
-
-props.setProperty('apiFrameworkVersion', version)
-props.store(propsFile.newWriter(), null)
 
 
