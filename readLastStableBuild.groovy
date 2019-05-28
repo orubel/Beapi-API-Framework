c12cab16ded37ce1ccf7f9da951bbb517aaf8bea
Owen Rubel <orubel@gmail.com>
2019-05-28 11:42:01 -0700

    test env stats

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 String userHome = System.properties['user.home']
-
+println(userHome)
 def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
 def patch = System.getenv('BUILD_NUMBER')
 def version = "${appVersion}.${patch}"
