b7fec91a787a7e137acb1b659ca8f508c9e34610
Owen Rubel <orubel@gmail.com>
2019-05-28 11:45:09 -0700

    update to use each individuals home env for script

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
 String userHome = System.properties['user.home']
-println(userHome)
+
 def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
 def patch = System.getenv('BUILD_NUMBER')
 def version = "${appVersion}.${patch}"
 
 
 Properties props = new Properties()
-def propsFile = new File("/home/orubel/.jenkins/workspace/beapi-backend/gradle.properties")
+def propsFile = new File("/${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
 props.load(propsFile.newDataInputStream())
 
 props.setProperty('apiFrameworkVersion', version)
