05138500e5762d70d1e6cbd47371e6bee993d3b2
Owen Rubel <orubel@gmail.com>
2019-06-13 15:40:54 -0700

    test

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -8,17 +8,17 @@ def version = "${appVersion}.${patch}"
 
 
 Properties props = new Properties()
-def propsFile = new File("/${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
+def propsFile = new File("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
 props.load(propsFile.newDataInputStream())
-println("### writing to '/${userHome}/.jenkins/workspace/beapi-backend/gradle.properties' > ${version}")
+println("### writing to '${userHome}/.jenkins/workspace/beapi-backend/gradle.properties' > ${version}")
 props.setProperty('apiFrameworkVersion', version)
 props.store(propsFile.newWriter(), null)
 
 
 Properties props2 = new Properties()
-def propsFile2 = new File("/${userHome}/.jenkins/workspace/api-framework/gradle.properties")
+def propsFile2 = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
 props2.load(propsFile2.newDataInputStream())
-println("### writing to '/${userHome}/.jenkins/workspace/api-framework/gradle.properties' > ${patch}")
+println("### writing to '${userHome}/.jenkins/workspace/api-framework/gradle.properties' > ${patch}")
 props2.setProperty('patchVersion', patch)
 props2.store(propsFile2.newWriter(), null)
 
