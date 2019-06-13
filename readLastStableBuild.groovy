ef1f209d444a95ff5ec383eb04d560996e12243f
Owen Rubel <orubel@gmail.com>
2019-06-13 16:36:38 -0700

    test

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-
+import org.apache.commons.configuration2.PropertiesConfiguration
 
 println "ls -all".execute().text
 
@@ -10,6 +10,11 @@ def patch = System.getenv('BUILD_NUMBER')
 def version = "${appVersion}.${patch}"
 
 
+PropertiesConfiguration config = new PropertiesConfiguration("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
+config.setProperty('apiFrameworkVersion', version)
+config.save()
+
+/*
 Properties props = new Properties()
 def propsFile = new File("${userHome}/.jenkins/workspace/beapi-backend/gradle.properties")
 props.load(propsFile.newDataInputStream())
@@ -21,16 +26,22 @@ try {
 } catch (IOException ex) {
 	println(ex)
 }
+*/
 
+PropertiesConfiguration config = new PropertiesConfiguration("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
+config.setProperty('patchVersion', patch)
+config.save()
 
+/*
 Properties props2 = new Properties()
 def propsFile2 = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
 props2.load(propsFile2.newDataInputStream())
 println("### writing to '${userHome}/.jenkins/workspace/api-framework/gradle.properties' > ${patch}")
 props2.setProperty('patchVersion', patch)
+OutputStream out = new FileOutputStream( f );
 try {
 	props2.store(propsFile2.newWriter(), null)
 } catch (IOException ex) {
 	println(ex)
 }
-
+*/
