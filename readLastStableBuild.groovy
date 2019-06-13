73432e1b27d0b3f00e2e8976f38f82d36ecb2fee
Owen Rubel <orubel@gmail.com>
2019-06-13 15:56:22 -0700

    test

--- readLastStableBuild.groovy
+++ readLastStableBuild.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
+import java.io.IOException
+
 String userHome = System.properties['user.home']
 
 def appVersion = (System.getenv('BEAPI_BUILD_VERSION'))?System.getenv('BEAPI_BUILD_VERSION'):'1'
@@ -13,6 +15,11 @@ props.load(propsFile.newDataInputStream())
 println("### writing to '${userHome}/.jenkins/workspace/beapi-backend/gradle.properties' > ${version}")
 props.setProperty('apiFrameworkVersion', version)
 props.store(propsFile.newWriter(), null)
+try {
+	props.store(propsFile.newWriter(), null)
+} catch (IOException ex) {
+	println(ex)
+}
 
 
 Properties props2 = new Properties()
@@ -20,5 +27,9 @@ def propsFile2 = new File("${userHome}/.jenkins/workspace/api-framework/gradle.p
 props2.load(propsFile2.newDataInputStream())
 println("### writing to '${userHome}/.jenkins/workspace/api-framework/gradle.properties' > ${patch}")
 props2.setProperty('patchVersion', patch)
-props2.store(propsFile2.newWriter(), null)
+try {
+	props2.store(propsFile2.newWriter(), null)
+} catch (IOException ex) {
+	println(ex)
+}
 
