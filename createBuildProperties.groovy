4664cd4ee4ab56427de7dfd5684ad12359745f82
Owen Rubel <orubel@gmail.com>
2019-06-14 08:14:06 -0700

    test

--- createBuildProperties.groovy
+++ createBuildProperties.groovy
@@ -20,7 +20,7 @@ FileInputStream in2 = new FileInputStream("${userHome}/.jenkins/workspace/api-fr
 
 props2.load(in2)
 props2.setProperty('patchVersion', patch)
-props2.store(in2, null)
+props2.store(out2, null)
 in2.close()
 
 
