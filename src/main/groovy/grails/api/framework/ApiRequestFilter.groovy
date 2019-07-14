ab18f01dd6c3027482b968ad4dce5039f624cefb
Owen Rubel <orubel@gmail.com>
2019-07-14 08:04:50 -0700

    further consolidation of filters

--- src/main/groovy/grails/api/framework/ApiRequestFilter.groovy
+++ src/main/groovy/grails/api/framework/ApiRequestFilter.groovy
@@ -10,6 +10,15 @@
 
 package grails.api.framework;
 
+
+
+
+import org.springframework.http.HttpStatus
+import grails.util.Environment
+
+
+
+
 import grails.plugin.springsecurity.rest.RestAuthenticationProvider
 import grails.plugin.springsecurity.rest.authentication.RestAuthenticationEventPublisher
 import grails.plugin.springsecurity.rest.token.AccessToken
@@ -57,6 +66,7 @@ import grails.compiler.GrailsCompileStatic
 class ApiRequestFilter extends GenericFilterBean {
 
     String headerName
+    String loginUri
 
     RestAuthenticationProvider restAuthenticationProvider
 
@@ -80,58 +90,160 @@ class ApiRequestFilter extends GenericFilterBean {
 
         AccessToken accessToken
 
-        try {
-            accessToken = tokenReader.findToken(httpRequest)
-            if (accessToken) {
+        if(!processPreflight(httpRequest, httpResponse) ) {
+            try {
+                accessToken = tokenReader.findToken(httpRequest)
+                if (accessToken) {
 
-                //log.debug "Token found: ${accessToken.accessToken}"
+                    //log.debug "Token found: ${accessToken.accessToken}"
 
-                accessToken = restAuthenticationProvider.authenticate(accessToken) as AccessToken
+                    accessToken = restAuthenticationProvider.authenticate(accessToken) as AccessToken
 
-                if (accessToken.authenticated) {
-                    //log.debug "Token authenticated. Storing the authentication result in the security context"
-                    //log.debug "Authentication result: ${accessToken}"
-                    SecurityContextHolder.context.setAuthentication(accessToken)
+                    if (accessToken.authenticated) {
+                        //log.debug "Token authenticated. Storing the authentication result in the security context"
+                        //log.debug "Authentication result: ${accessToken}"
+                        SecurityContextHolder.context.setAuthentication(accessToken)
 
-                    //println(accessToken.authenticated)
-                    //println(accessToken.authorities)
-                    //println(accessToken.expiration)
-                    //println(accessToken.refreshToken)
-                    //println(accessToken)
+                        //println(accessToken.authenticated)
+                        //println(accessToken.authorities)
+                        //println(accessToken.expiration)
+                        //println(accessToken.refreshToken)
+                        //println(accessToken)
 
-                    //authenticationEventPublisher.publishAuthenticationSuccess(accessToken)
+                        //authenticationEventPublisher.publishAuthenticationSuccess(accessToken)
 
-                    processFilterChain(httpRequest, httpResponse, chain, accessToken)
-                }else{
-                    //log.debug('not authenticated')
+                        processFilterChain(httpRequest, httpResponse, chain, accessToken)
+                    } else {
+                        //log.debug('not authenticated')
+                        httpResponse.setContentType("application/json")
+                        httpResponse.setStatus(401)
+                        httpResponse.getWriter().write('Token Unauthenticated. Uauthorized Access.')
+                        httpResponse.writer.flush()
+                        //return
+                    }
+
+                } else {
+                    //log.debug('token not found')
                     httpResponse.setContentType("application/json")
                     httpResponse.setStatus(401)
-                    httpResponse.getWriter().write('Token Unauthenticated. Uauthorized Access.')
+                    httpResponse.getWriter().write('Token not found. Unauthorized Access.')
                     httpResponse.writer.flush()
-                    //return
+                    return
                 }
 
-            } else {
-                //log.debug('token not found')
+            } catch (AuthenticationException ae) {
+                // NOTE: This will happen if token not found in database
+                //log.debug('Token not found in database.')
                 httpResponse.setContentType("application/json")
                 httpResponse.setStatus(401)
-                httpResponse.getWriter().write('Token not found. Unauthorized Access.')
+                httpResponse.getWriter().write('Token not found in database. Authorization Attempt Failed')
                 httpResponse.writer.flush()
-                return
+
+                //authenticationEventPublisher.publishAuthenticationFailure(ae, accessToken)
+                //authenticationFailureHandler.onAuthenticationFailure(request, response, ae)
             }
-
-        } catch (AuthenticationException ae) {
-            // NOTE: This will happen if token not found in database
-            //log.debug('Token not found in database.')
-            httpResponse.setContentType("application/json")
-            httpResponse.setStatus(401)
-            httpResponse.getWriter().write('Token not found in database. Authorization Attempt Failed')
-            httpResponse.writer.flush()
-
-            //authenticationEventPublisher.publishAuthenticationFailure(ae, accessToken)
-            //authenticationFailureHandler.onAuthenticationFailure(request, response, ae)
         }
 
+    }
+
+    boolean processPreflight(HttpServletRequest request, HttpServletResponse response) {
+
+        boolean options = 'OPTIONS' == request.method.toUpperCase()
+
+        //request.getHeader('Access-Control-Request-Headers')
+        //List headers = request.getHeaderNames() as List
+
+        /**
+         * First get CORS Network grps, then test the domains listed in the users network group
+         * against sent origin
+         */
+        this.loginUri = getLoginUrl()
+        String actualUri = request.requestURI - request.contextPath
+        String entryPoint = Metadata.current.getProperty(Metadata.APPLICATION_VERSION, String.class)
+        String controller
+        String action
+        String version
+
+        // TODO: need to also check for logoutUri
+        switch(actualUri) {
+            case ~/\/.{0}[a-z]${entryPoint}(-[0-9])*\/(.*)/:
+                String[] params = actualUri.split('/')
+                String[] temp = ((String)params[1]).split('-')
+                version = (temp.size()>1) ? temp[1].toString() : ''
+                controller = params[2]
+                action = params[3]
+                break
+            case loginUri:
+                String[] params = actualUri.split('/')
+                controller = params[1]
+                action = params[2]
+                break;
+            default:
+                response.setContentType("application/json")
+                response.setStatus(401)
+                response.getWriter().write("Bad URI Access attempted at '${actualUri}'")
+                response.writer.flush()
+                return true
+        }
+
+        // get users network group and config NetworkGroups list and test if networkGrp sent exists
+        List networkGroups = (List) Holders.grailsApplication.config.apitoolkit['networkGroups']
+        String networkGroupType = getNetworkGrp(version, controller, action, request, response)
+        if(!networkGroups.contains(networkGroupType)){
+            response.setContentType("application/json")
+            response.setStatus(401)
+            response.getWriter().write("NETWORKGRP for IO State file :"+controller+" cannot be found. Please double check it against available NetworkGroups in the beapi_api.yml config file.")
+            response.writer.flush()
+            return true
+        }else {
+
+            // Now we get networkGrpList of allowedOrigins and compare against sent Origin header
+            Map corsInterceptorConfig = (Map) Holders.grailsApplication.config.apitoolkit['corsInterceptor']
+
+            String[] includeEnvironments = corsInterceptorConfig['includeEnvironments'] ?: null
+            String[] excludeEnvironments = corsInterceptorConfig['excludeEnvironments'] ?: null
+            LinkedHashMap allowedOrigins = corsInterceptorConfig['networkGroups'] ?: null
+            String[] networkGroupList = allowedOrigins[networkGroupType]
+
+            if (excludeEnvironments && excludeEnvironments.contains(Environment.current.name)) {
+                // current env is excluded
+                // skip
+                return false
+            } else if (includeEnvironments && !includeEnvironments.contains(Environment.current.name)) {
+                // current env is not included
+                // skip
+                return false
+            }
+
+            String origin = request.getHeader('Origin')
+            if (options) {
+                response.addHeader('Allow', 'GET, HEAD, POST, PUT, DELETE, TRACE, PATCH, OPTIONS')
+                if (origin != 'null') {
+                    //response.setHeader("Access-Control-Allow-Headers", "Cache-Control,  Pragma, WWW-Authenticate, Origin, X-Requested-With, authorization, Content-Type,Access-Control-Request-Headers,Access-Control-Request-Method,Access-Control-Allow-Credentials")
+                    response.addHeader('Access-Control-Allow-Headers', 'Accept, Accept-Charset, Accept-Datetime, Accept-Encoding, Accept-Ext, Accept-Features, Accept-Language, Accept-Params, Accept-Ranges, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Expose-Headers, Access-Control-Max-Age, Access-Control-Request-Headers, Access-Control-Request-Method, Age, Allow, Alternates, Authentication-Info, Authorization, C-Ext, C-Man, C-Opt, C-PEP, C-PEP-Info, CONNECT, Cache-Control, Compliance, Connection, Content-Base, Content-Disposition, Content-Encoding, Content-ID, Content-Language, Content-Length, Content-Location, Content-MD5, Content-Range, Content-Script-Type, Content-Security-Policy, Content-Style-Type, Content-Transfer-Encoding, Content-Type, Content-Version, Cookie, Cost, DAV, DELETE, DNT, DPR, Date, Default-Style, Delta-Base, Depth, Derived-From, Destination, Differential-ID, Digest, ETag, Expect, Expires, Ext, From, GET, GetProfile, HEAD, HTTP-date, Host, IM, If, If-Match, If-Modified-Since, If-None-Match, If-Range, If-Unmodified-Since, Keep-Alive, Label, Last-Event-ID, Last-Modified, Link, Location, Lock-Token, MIME-Version, Man, Max-Forwards, Media-Range, Message-ID, Meter, Negotiate, Non-Compliance, OPTION, OPTIONS, OWS, Opt, Optional, Ordering-Type, Origin, Overwrite, P3P, PEP, PICS-Label, POST, PUT, Pep-Info, Permanent, Position, Pragma, ProfileObject, Protocol, Protocol-Query, Protocol-Request, Proxy-Authenticate, Proxy-Authentication-Info, Proxy-Authorization, Proxy-Features, Proxy-Instruction, Public, RWS, Range, Referer, Refresh, Resolution-Hint, Resolver-Location, Retry-After, Safe, Sec-Websocket-Extensions, Sec-Websocket-Key, Sec-Websocket-Origin, Sec-Websocket-Protocol, Sec-Websocket-Version, Security-Scheme, Server, Set-Cookie, Set-Cookie2, SetProfile, SoapAction, Status, Status-URI, Strict-Transport-Security, SubOK, Subst, Surrogate-Capability, Surrogate-Control, TCN, TE, TRACE, Timeout, Title, Trailer, Transfer-Encoding, UA-Color, UA-Media, UA-Pixels, UA-Resolution, UA-Windowpixels, URI, Upgrade, User-Agent, Variant-Vary, Vary, Version, Via, Viewport-Width, WWW-Authenticate, Want-Digest, Warning, Width, xsrf-token, X-Content-Duration, X-Content-Security-Policy, X-Content-Type-Options, X-CustomHeader, X-DNSPrefetch-Control, X-Forwarded-For, X-Forwarded-Port, X-Forwarded-Proto, X-Frame-Options, X-Modified, X-OTHER, X-PING, X-PINGOTHER, X-Powered-By, X-Requested-With')
+                    response.addHeader('Access-Control-Allow-Methods', 'POST, PUT, DELETE, TRACE, PATCH, OPTIONS')
+                    response.addHeader('Access-Control-Expose-Headers', 'xsrf-token, Location,X-Auth-Token')
+                    response.addHeader('Access-Control-Max-Age', '3600')
+                }
+            }
+
+
+            if (networkGroupList && networkGroupList.contains(origin)) { // request origin is on the white list
+                // add CORS access control headers for the given origin
+                response.setHeader('Access-Control-Allow-Origin', origin)
+                response.addHeader('Access-Control-Allow-Credentials', 'true')
+            } else if (!networkGroupList) { // no origin; white list
+                // add CORS access control headers for all origins
+                if (origin) {
+                    response.setHeader('Access-Control-Allow-Origin', origin)
+                    response.addHeader('Access-Control-Allow-Credentials', 'true')
+                } else {
+                    response.addHeader('Access-Control-Allow-Origin', '*')
+                }
+            }
+        }
+        response.status = HttpStatus.OK.value()
+        return options
     }
 
     boolean doesContentTypeMatch(HttpServletRequest request){
@@ -168,6 +280,58 @@ class ApiRequestFilter extends GenericFilterBean {
             return false
         }catch(Exception e){
             throw new Exception("[ApiRequestFilter :: getContentType] : Exception - full stack trace follows:",e)
+        }
+    }
+
+    @CompileDynamic
+    String getLoginUrl(){
+        return Holders.grailsApplication.config.grails.plugin.springsecurity.rest.login.endpointUrl
+    }
+
+    @CompileDynamic
+    String getNetworkGrp(String version, String controller, String action, HttpServletRequest request, HttpServletResponse response){
+        // login URI is always public; this is also handled by 3rd party plugin
+        //String loginUrl = getLoginUrl()
+        if("/${controller}/${action}" == this.loginUri){
+            return 'public'
+        }
+
+        ApplicationContext ctx = Holders.grailsApplication.mainContext
+        if(ctx) {
+            GrailsCacheManager grailsCacheManager = ctx.getBean('grailsCacheManager') as GrailsCacheManager
+            LinkedHashMap cache = [:]
+            def temp = grailsCacheManager?.getCache('ApiCache')
+            List cacheNames = temp.getAllKeys() as List
+            def tempCache
+            for (it in cacheNames) {
+                String cKey = it.simpleKey.toString()
+                if (cKey == controller) {
+                    tempCache = temp.get(it)
+                    break
+                }
+            }
+
+            def cache2
+            if (tempCache) {
+                cache2 = tempCache.get() as LinkedHashMap
+                version = (version.isEmpty()) ? cache2['cacheversion'] : version
+
+                if (!cache2?."${version}"?."${action}") {
+                    response.status = 401
+                    response.setHeader('ERROR', 'IO State Not properly Formatted for this URI. Please contact the Administrator.')
+                    response.writer.flush()
+                    return
+                } else {
+                    def session = RCH.currentRequestAttributes().getSession()
+                    session['cache'] = cache2
+                    return session['cache'][version][action]['networkGrp']
+                }
+            } else {
+                //println("no cache found")
+            }
+
+        }else{
+            //log.debug('no ctx found')
         }
     }
 
