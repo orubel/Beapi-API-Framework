0b6ddecd63dd90c4d546e685792981f420488583
Owen Rubel <orubel@gmail.com>
2019-12-17 07:09:57 -0800

    testing pipeline

--- createBuildProperties.groovy
+++ createBuildProperties.groovy
@@ -6,13 +6,15 @@ String userHome = System.properties['user.home']
 Properties props = new Properties()
 
 // update 3.x gradle.properties
-def propsFile = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties.3.x")
+def propsFile = new File("/var/lib/jenkins/workspace/api-framework/gradle.properties")
 props.load(propsFile.newDataInputStream())
 
 def appVersion = props.getProperty('buildVersion')
-def patch = System.getenv('BUILD_NUMBER')
+def patch = (System.getenv('BUILD_NUMBER'))?System.getenv('BUILD_NUMBER'):props.getProperty('patchVersion')
 def version = "${appVersion}"
+def job = System.getenv('JOB_NAME')
 
+println("job:"+job)
 println("patch:"+patch)
 println("version:"+version)
 
@@ -22,11 +24,11 @@ props.store(propsFile.newWriter(), null)
 
 
 // update default gradle.properties
-def propsFile2 = new File("${userHome}/.jenkins/workspace/api-framework/gradle.properties")
+def propsFile2 = new File("/var/lib/jenkins/workspace/api-framework/gradle.properties")
 props.load(propsFile2.newDataInputStream())
 
 appVersion = props.getProperty('buildVersion')
-patch = System.getenv('BUILD_NUMBER')
+patch = (System.getenv('BUILD_NUMBER'))?System.getenv('BUILD_NUMBER'):props.getProperty('patchVersion')
 version = "${appVersion}"
 
 println("patch:"+patch)
