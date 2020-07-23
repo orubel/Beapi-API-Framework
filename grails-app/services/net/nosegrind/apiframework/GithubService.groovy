3708988b9b221450b3ca8a1de492ff3f4dc3ee72
Owen Rubel <orubel@gmail.com>
2020-07-23 09:19:41 -0700

    security update

---
+++ grails-app/services/net/nosegrind/apiframework/GithubService.groovy
@@ -0,0 +1,27 @@
+package net.nosegrind.apiframework
+
+
+class GithubService {
+
+/** NOTICE::
+Since I have ALOT of lurkers who don't download but don't contribute, I wanted to say hi...
+
+I wanted to personally thank all of you using and downloading the code especially the OCI/Grails team and 
+all their enterprise customers the BeAPI project supports.
+
+As you know we have had Facebook make overtures to try to steal our work and now it seems our user base got 
+rattled and wants to either get the code for themselves or make sure that it STAYS OPEN.
+
+The surest way you can make sure the code stays open is through support: donate, star the project, and contribute.
+
+Otherwise yes, without support, open source projects eventually do move towards proprietary ends.
+ 
+This is one of many reasons why we keep separate code branchs for PRO versions which we implement for paid clients.
+
+So if you wish to keep this project open, please contribute.
+
+Thanks :)
+ 
+*/
+
+}
